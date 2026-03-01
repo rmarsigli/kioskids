27a2355160dd2a6f2e6d3d9c881c5f81b7479091
Rafhael Marsigli <rmarsigli@gmail.com>
2026-03-01 19:28:59 -0300

    fix: vitest alias, builder yml cleanup

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vitest/config'
 import { resolve } from 'path'
 
 export default defineConfig({
-  test: {
-    environment: 'node',
-    include: ['src/**/__tests__/**/*.test.ts'],
+  resolve: {
     alias: {
       '@shared': resolve(__dirname, 'src/shared'),
     },
+  },
+  test: {
+    environment: 'node',
+    include: ['src/**/__tests__/**/*.test.ts'],
   },
 })
